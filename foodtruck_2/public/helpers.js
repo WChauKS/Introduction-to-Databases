278ec94b4c664cc5588daeb08e2ab2bb7b12cf0a
halsam <wchauks@gmail.com>
2019-12-06 22:27:26 -0500

    Update helpers.js

--- foodtruck_2/public/helpers.js
+++ foodtruck_2/public/helpers.js
@@ -1,3 +1,6 @@
+// helper function for handlebars to process default selection in the Modify Truck Schedule table on truckschedule.handlebars
+// checks to see if the value matches, if true then it is the default selection
+// Result: page loads with drop downs correctly populated
 module.exports = {
     select: function (selected, option) {
     	console.log(selected, option);
